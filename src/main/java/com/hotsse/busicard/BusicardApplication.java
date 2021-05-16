2617dab2488aed788edc490da2c5e6c6851f0083
Hotsse <dkdlrja@naver.com>
2021-05-16 10:25:13 +0900

    project initialize

---
+++ src/main/java/com/hotsse/busicard/BusicardApplication.java
@@ -0,0 +1,13 @@
+package com.hotsse.busicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusicardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusicardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotsse/busicard/BusicardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotsse.busicard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusicardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
