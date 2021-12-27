0bcffb2b2660db6b3f5cd54974df1d771db4bf67
Hotsse <dkdlrja@naver.com>
2021-12-27 20:04:46 +0900

    Reset Test Code

--- src/test/java/com/hotsse/busicard/BusicardApplicationTests.java
+++ src/test/java/com/hotsse/busicard/BusicardApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BusicardApplicationTests {
 		
 	@Test
-	void contextLoads() throws Exception {
-		
+	void contextLoads() throws Exception {		
 	}
 }
